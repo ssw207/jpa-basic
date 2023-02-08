bd1c3285a4981c267f75e77913a60bf2bbc3e38d
swsong <ssw890720@gmail.com>
2023-02-08 20:43:38 +0900

    test : 낙관적 락 테스트

--- src/main/java/com/example/jpabasic/repository/CarRepository.java
+++ src/main/java/com/example/jpabasic/repository/CarRepository.java
@@ -10,10 +10,10 @@ import javax.persistence.LockModeType;
 public interface CarRepository extends JpaRepository<Car, Long> {
 
     /*
-        @Lock(LockModeType.NONE) : Lock 설저이 없더라도 @Version이 적용된 필드가 엔티티에 있으면 자동으로 낙관적락을 적용한다.
+        Lock 설저이 없더라도 @Version이 적용된 필드가 엔티티에 있으면 자동으로 낙관적락을 적용한다.
         업데이트 시점에 버전이 증가하고 업데이트시 조회시점 버전과 다르면 에러가 발생한다.
+        조회한 엔티티가 트랜잭션이 커밋될 때까지 다른 트랜잭션에 의해 변경되지 않음을 보장한다.
      */
-//    @Query("select c from car c where c.id = :id")
-    @Lock(LockModeType.OPTIMISTIC) // 낙관적 락 사용. 
+    @Lock(LockModeType.OPTIMISTIC)
     Car findLockById(@Param("id") Long id);
 }
